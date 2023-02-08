dfc3002e9505923742351ebe5ac6a9f60e1f0f05
davvidkrefft <davvidkrefft@gmail.com>
2023-02-08 20:20:25 +0100

    initial commit, create server

---
+++ server/controllers/users.js
@@ -0,0 +1,52 @@
+import User from '../models/User.js';
+
+// Clean Code
+const sendFormattedFriends = async user => {
+  const friends = await Promise.all(user.friends.map(id => User.findById(id)));
+  const formattedFriends = friends.map(
+    ({ _id, firstName, lastName, profession, location, picturePath }) => {
+      return { _id, firstName, lastName, profession, location, picturePath };
+    },
+  );
+  res.status(200).json(formattedFriends);
+};
+/* Read */
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    sendFormattedFriends(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+/* Update */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter(id => id !== friendId);
+      friend.friends = friend.friends.filter(id => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    sendFormattedFriends(user);
+  } catch (err) {}
+};
---
+++ server/index.js
@@ -0,0 +1,70 @@
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import multer from 'multer';
+import path from 'path';
+
+import { createPost } from './controllers/posts.js';
+import { register } from './controllers/auth.js';
+import { verifyToken } from './middleware/auth.js';
+import authRoutes from './routes/auth.js';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+/* Configurations */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* File Storage */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* Routes with files */
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/* Routes */
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+/* Mongoose */
+const PORT = process.env.PORT || 6001;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // for fast data import → one time and comment
+    // await User.insertMany(users);
+    // await Post.insertMany(posts);
+  } catch (error) {
+    throw console.log(`${error} connection failed`);
+  }
+};
+connect();
